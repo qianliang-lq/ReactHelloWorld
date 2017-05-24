25c7251a3d4fb648243ff37e05dad46f5a527815
Charles <charles.liangqian@gmail.com>
2017-05-23 21:49:34 -0400

    Simplest react first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div style={{float:'left'}}>
+                <ul style={{display:'inline-block'}}>
+                    <li>TOUR</li>
+                    <li>CUSTOMERS</li>
+                    <li>NEW FEATURES</li>
+                    <li>ENTERPRISE</li>
+                    <li>BLOG</li>
+                    <li>LOGIN</li>
+                    <li>SIGN UP FREE</li>
+
+                </ul>
+
+
+            </div>
+        );
+    }
+}
---
+++ client/components/Brand.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Brand extends React.Component{
+    render(){
+        return(
+
+            <h1>hello worlddasdfdsfdsaf</h1>
+
+        )
+
+    }
+}
